82bcd658747319cdb27663f2e9f2d8d1a1684d0b
Jonathan Lam <jonathanryanlam@gmail.com>
2015-01-22 12:20:17 -0500

    Step 5 introductions

--- person.rb
+++ person.rb
@@ -5,14 +5,22 @@ class Person #outerclass
 		end
 end
 
-	class Student
+	class Student < Person
 		def learn
 			puts "I get it!"
 		end
+
+		def student_intro
+			puts "Hi, my name is #{@name}, and I am a student"
+		end
 	end
 
-	class Instructor
+	class Instructor < Person
 		def teach
 			puts "Everything in Ruby is an Object."
+		end
+
+		def instructor_intro
+			puts "Hi, my name is #{@name} and I am an instructor"
 		end
 	end
